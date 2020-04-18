8d40734c8f32ee4cadf6eaad6472f076d91147b8
Alan Gonzalez <argaalan@hotmail.com>
2020-04-18 15:56:04 -0600

    Pictures tested and now working

--- view/materiales.php
+++ view/materiales.php
@@ -1,11 +1,14 @@
+<!--INITIALIZE FOR THIS PAGE-->
 <?php require_once('../functions/initialize.php'); ?>
 <?php
 $materiales = find_all_materials();
 ?>
 <?php $page_title = 'Materiales'; ?>
 <?php include(PAGES_PATH . '/staff_header.php'); ?>
-<div id="materiales">
+<!--INITIALIZE FOR THIS PAGE-->
 
+<div id="materiales">
+    <h1>Lista de materiales</h1>
     <table class="list">
         <tr>
             <th>Codigo de barras</th>
@@ -17,10 +20,13 @@ $materiales = find_all_materials();
             <tr>
                 <td><?php echo h($material['codigo_barras']); ?></td>
                 <td><?php echo h($material['nombre']); ?></td>
-                <td><?php echo h($material['foto']); ?></td>
+                <td><?php echo "<img src=\"" . h($material['foto']) . "\" alt=\"Image\" height=\"42\" width=\"42\">"; ?></td>
             </tr>
         <?php } ?>
     </table>
 </div>
 
+
+<!--INITIALIZE FOR THIS PAGE-->
 <?php include(PAGES_PATH . '/staff_footer.php'); ?>
+<!--INITIALIZE FOR THIS PAGE-->
