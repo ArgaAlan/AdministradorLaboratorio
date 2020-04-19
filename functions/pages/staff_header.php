be1e7cccc513c32f13d41ac1ca055dcf0ef12aed
Alan Gonzalez <argaalan@hotmail.com>
2020-04-19 13:47:51 -0600

    Materiales styled

--- functions/pages/staff_header.php
+++ functions/pages/staff_header.php
@@ -9,23 +9,42 @@ if (!isset($page_title)) {
 
 <head>
   <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="../css/style.css" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <title><?php echo $page_title ?></title>
 </head>
 
 <body>
-  <!--NAVBAR DE DANIEL-->
-  <header class="w3-container w3-blue-grey" style="height: 100px">
-    <div style="float: left; padding-top: 1.1%; width: 10%">
-      <img alt="logo" src="../src/logo.jpeg" style="width:90%">
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <img src="../src/logo.jpeg" width="100" height="30" class="d-inline-block align-top" alt="">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="../index.php">Inicio <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $page_title . ".php" ?>"> <?php echo $page_title ?> </a>
+        </li>
+        <!--Aqui iran los demas departamentos-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Iniciar Sesion</button>
+      </form>
     </div>
-    <div style="float: left; padding: .8% 1% 1% 5%;width: 70%">
-      <h2 style="font-size: 1.8vw"><u><?php echo $page_title ?></u></h2>
-    </div>
-    <div style="float: right; padding-top: 1.6%; width: 20%">
-      <button style="font-size: .8vw" type="button">Iniciar sesión</button>
-    </div>
-  </header>
-  <!--NAVBAR DE DANIEL-->
+  </nav>
